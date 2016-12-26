f75ea7fae0fa6bdde1e501bc1f739b7aded62fb2
aodag <aodagx@gmail.com>
2016-12-27 03:01:05 +0900

    added static ui contents

---
+++ Tumblin.Web/Content/app.js
@@ -0,0 +1,32 @@
+﻿(function() {
+    var app = {
+        controller() {
+            var vm = {
+                posts: m.prop([])
+            };
+            m.request({
+                method: "GET",
+                url: "/api/posts"
+            }).then(vm.posts);
+            return vm;
+        },
+        view(ctrl) {
+            var posts = ctrl.posts();
+            return m("main", [
+                m("section", posts.map((post) => {
+                    return m("div", [
+                        m("h2", m("a", {href: post.id}, post.title)),
+                        m("p", post.text)
+                    ]);
+                }))
+            ]);
+        }
+    };
+    function init() {
+        m.mount(document.body, m.component(app));
+    }
+    function main() {
+        window.addEventListener("load", init);
+    }
+    main();
+}());
