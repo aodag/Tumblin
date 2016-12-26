711a622c50c525b5b9d5e3a7acfa229cdef5622e
aodag <aodagx@gmail.com>
2016-12-26 17:18:09 +0900

    added web project with nancy

---
+++ Tumblin.Web/TumblinModule.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Nancy;
+
+namespace Tumblin.Web
+{
+    public class TumblinModule: NancyModule
+    {
+        public TumblinModule()
+        {
+            Get["/"] = _ => "Hello";
+        }
+    }
+}
