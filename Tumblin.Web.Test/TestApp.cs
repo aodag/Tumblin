4b4cda871b8277c119f0a0ed98b5be32923a32ed
aodag <aodagx@gmail.com>
2016-12-27 00:38:15 +0900

    added testing facility

---
+++ Tumblin.Web.Test/TestApp.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using System.Configuration;
+
+namespace Tumblin.Web.Test
+{
+    [TestFixture]
+    public class TestApp
+    {
+        [Test]
+        public void TestIt()
+        {
+            var bootstrapper = new TumblinBootstrapper();
+            var browser = new Nancy.Testing.Browser(bootstrapper);
+            var response = browser.Get("/api/posts");
+        }
+
+        [Test]
+        public void TestFailure()
+        {
+            var connectionSettings = ConfigurationManager.ConnectionStrings["DefaultConnection"];
+            Assert.NotNull(connectionSettings);
+        }
+    }
+}
