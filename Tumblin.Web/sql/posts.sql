ed43dacb8595d03ff9f7285144675de9c7ab0739
aodag <aodagx@gmail.com>
2016-12-27 00:37:46 +0900

    added ddl

---
+++ Tumblin.Web/sql/posts.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE `posts` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) DEFAULT NULL,
+  `text` text,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
